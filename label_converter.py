1926efcf7d5c5c7115c2e6ef705a420b6c8c2f9b
Gregory Kramida <algomorph@gmail.com>
2016-08-09 18:04:09 -0400

    Tweaked the label_converter to work on label text files directly

--- label_converter.py
+++ label_converter.py
@@ -1,54 +1,80 @@
-'''
-JSON Label Generator for LSTM
-author - Chethan Mysore Parameshwara
-email - analogicalnexus159@gmail.com
-'''
-import csv
+"""
+text label -> JSON Label Converter for LSTM
+authors - Chethan Mysore Parameshwara, Greg Kramida
+emails - analogicalnexus159@gmail.com, algomorph@gmail.com
+"""
 import json
+import re
+import sys
+import numpy as np
 
 
 def csv2json(s_fid, e_fid, label):
     data = {
-    's_fid' : s_fid,
-    'e_fid' : e_fid,
-    'label' : label
-	}
+        's_fid': s_fid,
+        'e_fid': e_fid,
+        'label': label
+    }
     return data
 
-def jsonwrite(s_fid, e_fid, label):
-     with open('al0_wo5_labels.json', 'a') as f:
-            data = csv2json(s_fid, e_fid, label)
-            json.dump(data, f, indent=3)
-            f.write(',')
-            f.write('\n')
-            print (s_fid, e_fid, label)
+
+def jsonwrite(file, s_fid, e_fid, label):
+    data = csv2json(s_fid, e_fid, label)
+    json.dump(data, file, indent=3)
+    file.write(',')
+    file.write('\n')
+
 
 def read_csv(filename):
-	with open(filename) as csvfile:
-		readCSV = csv.reader(csvfile, delimiter=',')
-		s_fid = []
-		e_fid = []
-		l_fid = []
-		for row in readCSV:
-			s = int(row[0])
-			e = int(row[1])
-			l = int(row[2])
+    with open(filename) as csvfile:
+        readCSV = csv.reader(csvfile, delimiter=',')
+        s_fid = []
+        e_fid = []
+        l_fid = []
+        for row in readCSV:
+            s = int(row[0])
+            e = int(row[1])
+            l = int(row[2])
 
-			s_fid.append(s)
-			e_fid.append(e)
-			l_fid.append(l)
-		return s_fid, e_fid, l_fid
+            s_fid.append(s)
+            e_fid.append(e)
+            l_fid.append(l)
+        return s_fid, e_fid, l_fid
 
-#writer = csv.writer(open("label.csv", "w"), delimiter=',', lineterminator='\n')
-def main():	
-    [s_fid, e_fid, l_fid] = read_csv("al0_labels.csv")	
-    jsonwrite(1, s_fid[0]-1, 5)
-	
-    for x in range(0, 100):
-        jsonwrite(s_fid[x], e_fid[x], l_fid[x]) 
-        if e_fid[x] == s_fid[x+1]:
-            continue
-        else: 
-            jsonwrite(e_fid[x]+1,s_fid[x+1]-1, 5)
 
-main()
+# writer = csv.writer(open("label.csv", "w"), delimiter=',', lineterminator='\n')
+def main():
+    lines = tuple(open("al0_labels.txt", 'r'))
+    parse_re = re.compile(r"(\d+)\s*-(\d+):\s*(R|G|S|\[out of frame\])\s*?(\?)\s*?")
+    label_mapping = {
+        "X": 0,
+        "[out of frame]": 0,
+        "G": 1,
+        "R": 2,
+        "S": 3,
+    }
+    default_behavior_label = 4
+    sample_data = []
+    for line in lines:
+        match = parse_re.match(line)
+        if match:
+            if match.group(4) is None:
+                start = int(match.group(0))
+                end = int(match.group(1))
+                label = label_mapping[match.group(2)]
+            sample_data.append([start, end, label])
+
+    sample_data = np.array(sample_data, dtype=np.int32)
+    with open('al0_wo5_labels.json', 'a') as file:
+        if sample_data[0, 0] > 0:
+            jsonwrite(0, sample_data[0, 0] - 1, default_behavior_label)
+        for ix_sample in len(sample_data)-1:
+            sample = sample_data[ix_sample]
+            next_sample = sample_data[ix_sample+1]
+            jsonwrite(sample[0], sample[1], sample[2])
+            jsonwrite(sample[1] + 1, next_sample[0] - 1, default_behavior_label)
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
